32dafecb47ac222757c6c1a9522d8172efb20e74
Irina Lashitskaya <kuved@mail.ru>
2020-12-22 22:31:58 +0300

    homwork 16

---
+++ homwork_16/script.js
@@ -0,0 +1,83 @@
+var downloadButton = document.getElementsByName('downloadButton')[0],
+    bookmark = document.getElementsByClassName('tab')[0];
+
+downloadButton.addEventListener('click', clickDownloadButton);
+
+localStorage.clear();
+
+function clickDownloadButton() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://reqres.in/api/users?page=2', true);
+    xhr.send();
+    xhr.onload = function() {
+        var statusType = +String(this.status)[0],
+            answer,
+            newBookmark,
+            counter = 1,
+            divMain,
+            divFirstName,
+            divLastName,
+            divFoto;
+
+        if (statusType === 2) {
+            answer = JSON.parse(this.response).data;
+
+            for (var i = 0; i < answer.length; i++) {
+                newBookmark = document.createElement('button');
+                newBookmark.classList.add('tabLinks');
+                newBookmark.textContent = 'User ' + counter;
+                bookmark.appendChild(newBookmark);
+                newBookmark.addEventListener('click', openNewBookmark);
+
+                divMain = document.createElement('div');
+                divMain.classList.add('tabContent');
+                divMain.id = 'User' + counter;
+                document.body.appendChild(divMain);
+
+                divFoto = document.createElement('img');
+                divFoto.src = answer[i]['avatar'];
+                divMain.appendChild(divFoto);
+
+                divFirstName = document.createElement('div');
+                divFirstName.textContent = 'First Name: ' + answer[i]['first_name'];
+                divMain.appendChild(divFirstName);
+
+                divLastName = document.createElement('div');
+                divLastName.textContent = 'Last Name: ' + answer[i]['last_name'];
+                divMain.appendChild(divLastName);
+
+                if (counter === 1) {
+                    newBookmark.click();
+                }
+                counter++;
+            }
+        }
+    };
+
+    xhr.onerror = function() {
+        var divError = document.createElement('div');
+        divError.textContent = 'Ошибка! данные не получены!';
+        divError.classList.add('error');
+        document.body.appendChild(divError);
+    };
+}
+
+function openNewBookmark(event) {
+    var tabContent,
+        tabLinks,
+        num,
+        i;
+
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    num = this.textContent[this.textContent.length-1];
+    document.getElementById('User' + num).style.display = "block";
+    event.currentTarget.className += " active";
+}
